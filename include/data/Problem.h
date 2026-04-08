6e2998f904ad2e7b6198adb8634b6cc898d6ee03
Kunlin Yu <yukunlin@syriusrobotics.com>
2026-04-08 14:33:32 +0800

    Use full matrix in Problem
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- include/data/Problem.h
+++ include/data/Problem.h
@@ -9,7 +9,7 @@
 
 class Problem {
  private:
-  std::vector<WeightType> costs_;
+  std::vector<std::vector<WeightType>> costs_;
 
  public:
   ProblemType type = UNKNOW_PROBLEM;
@@ -25,27 +25,20 @@ class Problem {
   }
   void init(int dim, const std::vector<std::vector<WeightType>>& costs) {
     dimension = dim;
-    costs_.resize(dimension * (dimension - 1) / 2);
-    for (size_t i = 0; i < dimension; i++)
-      for (size_t j = 0; j < i; j++) costs_[i * (i - 1) / 2 + j] = costs[i][j];
+    costs_ = costs;
   }
   void init(int dim, const std::function<WeightType(size_t, size_t)>& costs) {
     dimension = dim;
-    costs_.resize(dimension * (dimension - 1) / 2);
-    for (size_t i = 0; i < dimension; i++)
-      for (size_t j = 0; j < i; j++) costs_[i * (i - 1) / 2 + j] = costs(i, j);
+    costs_.resize(dimension);
+    for (size_t i = 0; i < dimension; i++) {
+      costs_[i].resize(dimension);
+      for (size_t j = 0; j < i; j++) costs_[i][j] = costs(i, j);
+    }
   }
   WeightType get(size_t i, size_t j) {
     assert(i < dimension);
     assert(j < dimension);
-    if (i == j) return 0;
-    size_t min = std::min(i, j);
-    size_t max = std::max(i, j);
-    return costs_[max * (max - 1) / 2 + min];
-  }
-  void clear() {
-    costs_.clear();
-    costs_.shrink_to_fit();
+    return costs_[i][j];
   }
 };
 
