e567ff91bde6e25724751825ee6289965bd569dd
Kunlin Yu <yukunlin@syriusrobotics.com>
2026-04-10 14:14:28 +0800

    Do not access context in ExtractFinalTour
    Signed-off-by: Kunlin Yu <yukunlin@syriusrobotics.com>

--- src/WriteTour.cpp
+++ src/WriteTour.cpp
@@ -8,12 +8,12 @@
 #include "plog/Log.h"
 
 Tour ExtractFinalTour(const std::vector<NodeIdType>& ids) {
+  size_t dimension = ids.size() - 1;
   int i;
-  assert(context.dimension == ids.size() - 1);
-  for (i = 1; i < context.dimension && ids[i] != DepotIndex; i++);
+  for (i = 1; i < dimension && ids[i] != DepotIndex; i++);
   PLOGI << "i value : " << i;
-  bool Forward = ids[i < context.dimension ? i + 1 : 1] <
-                 ids[i > 1 ? i - 1 : context.dimension];
+  bool Forward = ids[i < dimension ? i + 1 : 1] <
+                 ids[i > 1 ? i - 1 : dimension];
   if (Forward) {
     LOGI << "Tour direction: forward";
   } else {
@@ -22,16 +22,16 @@ Tour ExtractFinalTour(const std::vector<NodeIdType>& ids) {
 
   Tour tour;
   i = 1;
-  for (int j = 1; j <= context.dimension; j++) {
+  for (int j = 1; j <= dimension; j++) {
     NodeIdType a = ids[i];
-    if (a <= context.dimension)
+    if (a <= dimension)
       tour.node_ids.push_back(a);
     else
       LOGE << "Warning: Node " << a
            << " is out of range and will be skipped in the tour file.";
     i += Forward ? 1 : -1;
-    if (i > context.dimension) i = 1;
-    if (i < 1) i = context.dimension;
+    if (i > dimension) i = 1;
+    if (i < 1) i = dimension;
   }
   return tour;
 }
